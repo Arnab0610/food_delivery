e7fadc30e6e9cbc1bc5192ed238b24b273be6f6e
Arnab Mondal <108365654+Arnab0610@users.noreply.github.com>
2025-04-29 10:24:23 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,35 @@
+    <footer class="bg-dark text-light py-4 mt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h5>About Us</h5>
+                    <p>We deliver delicious food right to your doorstep. Enjoy the best cuisines from top restaurants in your area.</p>
+                </div>
+                <div class="col-md-4">
+                    <h5>Quick Links</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="index.php" class="text-light">Home</a></li>
+                        <li><a href="menu.php" class="text-light">Menu</a></li>
+                        <li><a href="#" class="text-light">Contact Us</a></li>
+                        <li><a href="#" class="text-light">Terms & Conditions</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h5>Contact Info</h5>
+                    <ul class="list-unstyled">
+                        <li><i class="fas fa-phone"></i> +91 8768744627</li>
+                        <li><i class="fas fa-envelope"></i> info@fooddelivery.com</li>
+                        <li><i class="fas fa-map-marker-alt"></i> kolkata 700029</li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <div class="text-center">
+                <p class="mb-0">&copy; <?php echo date('Y'); ?> Food Delivery. All rights Arnab Mondal.</p>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</body>
+</html>
